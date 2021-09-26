f5af92d5808c217ef573d79ec9dad502471c3b11
jinsuSang <climbplant39@kakao.com>
2021-09-26 22:54:01 +0900

    practice02

---
+++ practice02/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    image = ProcessedImageField(
+        blank=True,
+        processors = [Thumbnail(200, 300),],
+        format='jpeg',
+        options = {'quality': 90},
+
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
